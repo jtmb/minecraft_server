d83b1d369a8751d19f8e739bfadb9c48b764a19c
jtmb <86915618+jtmb@users.noreply.github.com>
2021-09-17 16:43:04 -0400

    Add files via upload

--- localrunner.sh
+++ localrunner.sh
@@ -8,6 +8,9 @@ docker rm -f IPAA
 
 docker run -it -d -p 25565:25565 --name IPAA --hostname Minecraft_server_Alpine  -v /Users/brajam/OneDrive\ -\ Enterprise\ 365/Cloned\ Projects/Alpine_Project:/root/minecraft ubuntu:latest
 
+bash serversetup.sh
+
 docker exec -it IPAA bash
+
 
 
--- serversetup.sh
+++ serversetup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+docker exec -it IPAA cd minecraft_server
 
-wget https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
+docker exec -it IPAA wget https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
 
-mv server.jar minecraft_server.jar
+docker exec -it IPAA mv server.jar minecraft_server.jar
 
-java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
+docker exec -it IPAA java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
+
+docker exec -it IPAA cd ..
+
+docker exec -it IPAA cp  eula.txt /minecraft/minecraft_server
+
+docker exec -it IPAA 
 
 # COPY eula.txt /root/minecraft/minecraft
-
-java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
