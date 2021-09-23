fd0ca9ce34f673e005d2d63807f48910ca0f28cb
jtmb <86915618+jtmb@users.noreply.github.com>
2021-09-23 12:01:14 -0400

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-docker exec -it IPAA cd minecraft_server
+docker exec -it minecraft-server cd minecraft_server
 
-docker exec -it IPAA wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
+docker exec -it minecraft-server wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
 
-docker exec -it IPAA mv server.jar minecraft_server.jar
+docker exec -it minecraft-server mv server.jar minecraft_server.jar
 
-docker exec -it IPAA java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
+docker exec -it minecraft-server java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
 
-docker exec -it IPAA cd ..
+docker exec -it minecraft-server cd ..
 
-docker exec -it IPAA cp  eula.txt /minecraft/minecraft_server
+docker exec -it minecraft-server cp eula.txt /minecraft/minecraft_server
 
-docker exec -it IPAA 
+docker exec -it minecraft-server  
 
 # COPY eula.txt /root/minecraft/minecraft
