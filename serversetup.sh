47501b4775d70ac42d024ee96a40544c65fe445a
jtmb <86915618+jtmb@users.noreply.github.com>
2021-09-23 12:41:07 -0400

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-docker exec -it minecraft-server cd minecraft_server wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar && mv server.jar minecraft_server.jar && java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui && cd .. && cp eula.txt /minecraft && java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui && echo SERVER UP
+docker exec -it minecraft-server mkdir minecraft_server && cd minecraft_server
+docker exec -it minecraft-server cd minecraft_server && wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
+docker exec -it minecraft-server cd minecraft_server && mv server.jar minecraft_server.jar
+docker exec -it minecraft-server cd minecraft_server && java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui
+docker exec -it minecraft-server cp eula.txt /minecraft/minecraft_server
+docker exec -it minecraft-server cd minecraft_server && java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui
 
-# COPY eula.txt /root/minecraft/minecraft
+echo SERVER UP
+
+# COPY eula.txt /root/minecraft/min
