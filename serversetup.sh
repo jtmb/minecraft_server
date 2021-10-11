8aa28b49ed3b395878cbdd4113d48e4e5e0e0298
jtmb <86915618+jtmb@users.noreply.github.com>
2021-10-11 13:24:36 -0400

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker exec -it minecraft-server mkdir minecraft_server
-docker exec -it minecraft-server wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
-docker exec -it minecraft-server mv server.jar minecraft_server.jar
-docker exec -it minecraft-server java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui
-docker exec -it minecraft-server cp /root/minecraft/eulatrue/eula.txt /root/minecraft/
-docker exec -it minecraft-server java -Xmx4096M -Xms4096M -d -jar minecraft_server.jar nogui
+minecraft-server mkdir minecraft_server
+wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
+mv server.jar minecraft_server.jar
+java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui
+cp /root/minecraft/eulatrue/eula.txt /root/minecraft/
+java -Xmx4096M -Xms4096M -d -jar minecraft_server.jar nogui
 
 echo SERVER UP
 sudo ufw allow 25565
