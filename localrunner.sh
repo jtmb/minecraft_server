843ca89084e6886c48cebd8b73b0c2ace7fb7b15
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-21 16:33:53 -0500

    runner.sh

--- localrunner.sh
+++ localrunner.sh
@@ -1,18 +1,3 @@
-#!/bin/sh
-
-sudo docker pull ubuntu
-
-sudo docker build -t ubuntu:latest .
-
-sudo docker rm -f minecraft-server
-
-# docker run -it --hostname ipaclient.greatwestlifedigital.net --privileged --name dh_idm dh-idm-agent:latest bash
-
-sudo docker run -it -d -p 25565:25565 --name minecraft-server --hostname minecraft-server -v /home/uadmin/docker-container-data/minecraft_server:/root/minecraft ubuntu:latest
-
-sudo bash serversetup.sh
-
-sudo docker exec -it minecraft-server bash
-
-
-
+docker run -d -v /home/james/container-program-files/minecraft-server:/data \
+    -e TYPE=PAPER \
+    -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server
