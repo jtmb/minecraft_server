333b30f61490a3802d9f28d19daf71f05e3d0047
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-21 18:25:51 -0500

    Update localrunner.sh

--- localrunner.sh
+++ localrunner.sh
@@ -2,6 +2,7 @@ docker run -d -v /home/james/container-program-files/minecraft-server:/data \
     -e TYPE=PAPER \
     -e MEMORY=4G \
     -e MAX_MEMORY=4G \
+    -e EXEC_DIRECTLY=true \
     -e TZ=America/Toronto \
     -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server
 
