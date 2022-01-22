bceca843bb1abe7983f19bcc18e4ba8b3a8d31b3
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-22 08:42:49 -0500

    Update localrunner.sh

--- localrunner.sh
+++ localrunner.sh
@@ -6,7 +6,6 @@ docker run -d -v /home/james/container-program-files/fileserver_share_files/mine
     -e MAX_MEMORY=4G \
     -e MOTD="Defeating a sandwitch only makes it tastier." \
     -e EXEC_DIRECTLY=true \
-    -e TUNE_VIRTUALIZED=TRUE \
     -e TZ=America/Toronto \
     --cpus="4" \
     -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server
